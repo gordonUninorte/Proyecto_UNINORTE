022e104bc94b8f7c9598bb1334bf267e42ed454f
JesusCalvoMejia <jesuscalvomejia@gmail.com>
2021-10-21 22:28:26 -0500

    Select funcionando desde la BD

--- app.py
+++ app.py
@@ -1,129 +1,28 @@
-from flask import Flask, render_template, request
+##########################  APP CON VIEWS  ############
 
-app = Flask(__name__)
+import os
+from flask import Flask  
+
+def create_app():
+    """Función que crea la aplicación principal. 
+       Registra las plantillas de api y views(main) con sus respectivas url.
+       Registra un secrete Key con un texto fijo.
+       
+        Parameters:
+        Ninguno
+
+        Returns:
+        app de Flask
+
+    """
+    app = Flask(__name__)
+
+    app.secret_key = 'Equipo8'#os.urandom( 24 )
+
+    from views import main
 
 
-@app.route('/', methods=('GET', 'POST'))          	
-def hola_mundo():   
-
-    if(request.method=='POST'):
-        cad=""
+    app.register_blueprint(main, url_prefix='/')  
     
-        
-        cad= request.form['username']        
-        cad= cad + "-" +request.form['password']
-        if((request.form['username'] == 'jesus@equipo8.edu.co') & (request.form['password']=='Prueba123')): 
-            return render_template('home_admin.html')#'Hola, Mundo!'
-        elif((request.form['username'] == 'Lila@equipo8.edu.co') & (request.form['password']=='Prueba123')): 
-            return render_template('home_docentes.html')#'Hola, Mundo!'
-        elif((request.form['username'] == 'Marvin@equipo8.edu.co') & (request.form['password']=='Prueba123')): 
-            return render_template('home_estudiantes.html')#'Hola, Mundo!'
-        elif((request.form['username'] == 'Germania@equipo8.edu.co') & (request.form['password']=='Prueba123')): 
-            return render_template('home_docentes.html')#'Hola, Mundo!'
-        elif((request.form['username'] == 'Giam@equipo8.edu.co') & (request.form['password']=='Prueba123')): 
-            return render_template('home_estudiantes.html')#'Hola, Mundo!'
-        else:
-            return render_template('login.html')
-        
-    return render_template('login.html')#'Hola, Mundo!'	
+    return app 
 
-
-
-  ###############################   ADMIN  ###############################      
-@app.route('/home_admin', methods=('GET', 'POST'))
-def home_admin():
-    
-    return render_template("home_admin.html")
-
-##############################  GESTIONAR PERÍODO  ################################
-@app.route('/periodo', methods=('GET', 'POST'))
-def periodo():
-    
-    return render_template("ingresar_periodo.html")
-
-##############################  GESTIONAR MATERIA  ################################
-@app.route('/materia', methods=('GET', 'POST'))
-def materia():
-    
-    return render_template("ingresar_materia.html")
-
-##############################  GESTIONAR FACULTAD  ################################
-@app.route('/asignar_docentes', methods=('GET', 'POST'))
-def asignar_docentes():
-    
-    return render_template("asignar_docentes.html")
-
-
-##############################  GESTIONAR FACULTAD  ################################
-@app.route('/curso', methods=('GET', 'POST'))
-def grupo():
-    
-    return render_template("ingresar_grupo.html")
-
- ###############################  GESTIONAR ADMIN  ###############################    
-@app.route('/admin', methods=('GET', 'POST'))
-def admin():
-    
-    return render_template("ingresar_admin.html")
-
-@app.route('/edit_admin', methods=('GET', 'POST'))
-def edit_admin():
-    
-    return render_template("edit_admin.html")
-
-
-  ###############################  DOCENTES  #############################      
-@app.route('/home_docentes', methods=('GET', 'POST'))
-def home_docentes():
-    
-    return render_template("home_docentes.html")
-
-  ############################### INGRESAR DOCENTES  #############################      
-@app.route('/ingresar_docentes', methods=('GET', 'POST'))
-def ingresar_docentes():
-    
-    return render_template("ingresar_docentes.html")
-
-  ############################### EDIT DOCENTES  #############################      
-@app.route('/edit_docentes', methods=('GET', 'POST'))
-def edit_docentes():
-    
-    return render_template("edit_docentes.html")
-
- ############################### ASIGNAR ESTUDIANTE  #############################      
-@app.route('/ingresar_estudiante', methods=('GET', 'POST'))
-def ingresar_estudiante():
-    
-    return render_template("ingresar_estudiante.html")
-
-############################### EDIT ESTUDIANTE  #############################      
-@app.route('/edit_estudiante', methods=('GET', 'POST'))
-def edit_estudiante():
-    
-    return render_template("edit_estudiante.html")
-
-############################### CREAR ACTIVIDADES  #############################      
-@app.route('/Crear_actividades', methods=('GET', 'POST'))
-def Crear_actividades():
-    
-    return render_template("Crear_actividades.html")
-
-############################### EVALUAR ACTIVADES  #############################      
-@app.route('/evaluar_actividades', methods=('GET', 'POST'))
-def evaluar_actividades():
-    
-    return render_template("evaluar_actividades.html")
-############################### HOME ESTUDIANTE  #############################      
-@app.route('/home_estudiantes', methods=('GET', 'POST'))
-def home_estudiantes():
-    
-    return render_template("home_estudiantes.html")
-
-############################### VER CALIFICACIÓN  #############################      
-@app.route('/actividades_estu', methods=('GET', 'POST'))
-def actividades_estu():
-    
-    return render_template("actividades_estu.html")
-    
-if __name__== "__main__":
-    app.run(debug=True);
---
+++ static/js/ajax.js
@@ -0,0 +1,82 @@
+function cargarMensaje()
+{
+    var objXMLHTTP = new XMLHttpRequest();
+
+    objXMLHTTP.open('GET', 'http://127.0.0.1:5000/login');
+
+   objXMLHTTP.addEventListener('load', completado);
+   objXMLHTTP.addEventListener('error', manejarError);
+   objXMLHTTP.addEventListener('progress', progreso);
+   objXMLHTTP.addEventListener('abort', abortado);
+
+   objXMLHTTP.send();
+}
+
+
+function manejarError(evt)
+{
+    console.log('ocurrio un error.');
+}
+
+function progreso(evt)
+{
+    var procentaje = evt.loaded / evt.total * 100;
+    console.log(procentaje);
+}
+
+
+function abortado(evt)
+{
+    console.log('cancelado');
+}
+
+function completado(evt)
+{
+
+    var data =  JSON.parse(this.response);
+
+    console.log(data);
+
+    var info="";
+
+    for(var i =0; i < data.length; i++)
+    {
+        var mensaje = data[i];
+        info += "<div class= 'mensaje'><p class='asunto'>" +mensaje.asunto + "</p>"
+        info += "<p class='contenidoMensaje'>" +mensaje.mensaje + "</p>"
+        info += "<p class='usuario'>" +mensaje.usuario + "</p></div>"
+        
+    }
+
+   
+
+    
+
+    document.getElementById('respuesta').innerHTML= info;
+
+}
+
+/*----------------------------------------------------------------*/
+
+function guardarMensaje()
+{
+    var objXMLHTTP = new XMLHttpRequest();
+
+    objXMLHTTP.open('POST', 'http://127.0.0.1:5000/login');
+
+    objXMLHTTP.addEventListener('load', agregarCompletado);
+    
+    objXMLHTTP.setRequestHeader("Content-Type", "application/json")
+
+    objXMLHTTP.send(JSON.stringify({'usuario':'Pepe', 'asunto':'prueba', 'mensaje':'Hola mundo'}));
+}
+
+function agregarCompletado(evt)
+{
+    var data =  JSON.parse(this.response);
+
+    console.log(data);
+
+    cargarMensaje();
+
+}
